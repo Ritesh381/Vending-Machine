6cb14447d79fabaef7a5067e1588d6afa287d416
Ritesh381 <prajapatiritesh381@gmail.com>
2025-01-18 21:18:48 +0530

    Initial

---
+++ payment.js
@@ -0,0 +1,39 @@
+const pay = document.querySelector("#pay");
+const payment = document.querySelector(".payment");
+const paymentSuccess = document.querySelector(".payment-success");
+const paymentFailed = document.querySelector(".payment-failed");
+const main = document.querySelector(".default")
+const cancel = document.querySelector("#cancel")
+const mainMenu = document.querySelectorAll(".main-menu")
+const tryAgain = document.querySelector(".try-again")
+
+pay.addEventListener("click",()=>{
+    main.style.display = "none"
+    payment.style.display = "flex";
+    paymentSuccess.style.display = "none";
+    paymentFailed.style.display = "none";
+})
+cancel.addEventListener("click", ()=>{
+    main.style.display = "none"
+    payment.style.display = "none";
+    paymentSuccess.style.display = "none";
+    paymentFailed.style.display = "flex";
+})
+tryAgain.addEventListener("click", ()=>{
+    main.style.display = "none"
+    payment.style.display = "flex";
+    paymentSuccess.style.display = "none";
+    paymentFailed.style.display = "none";
+})
+mainMenu.forEach((butt)=>{
+    butt.addEventListener("click", ()=>{
+        main.style.display = "flex"
+        payment.style.display = "none";
+        paymentSuccess.style.display = "none";
+        paymentFailed.style.display = "none";
+    })
+})
+
+
+// let data = 
+// localStorage.setItem("product", JSON.stringify(data))
---
+++ script.js
@@ -0,0 +1,130 @@
+// Making the geid of vending machine
+const productContainer = document.querySelector('.product-container');
+const img = "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcTgGIV8N6Ra8O1NwpDMMEzzFwV6tHeogzpT2nv0Ajui13SIC1FvjOMaFntWeoIQ7zvFO6tyz_D3CaLoEf16pQpcDHQUUworQinoVmDTd9D16aiOzM876T1W"
+
+// Get items from localStorage
+let products = JSON.parse(localStorage.getItem('product'));
+if (products) {
+    console.log(products);
+    Object.keys(products).forEach((key) => {
+        const product = products[key];
+        makeProduct(key, product.price, product.image, product.quantity);
+    });
+    let empty = 24 - Object.keys(products).length;
+    for (let i = 0; i < empty; i++) {
+        makeProduct("", "0", "");
+    }
+
+} else {
+    console.log("No object found in localStorage.");
+    products = {};
+    let empty = 24;
+    for (let i = 0; i < empty; i++) {
+        makeProduct("", "0", "");
+    }
+}
+
+function makeProduct(id, price, image) {
+    const productTemplate = `
+    <img src="${image}" alt="empty">
+    <p id="price">₹ ${price}.00</p>
+    `;
+    const newItem = document.createElement("div");
+    newItem.classList.add("product");
+    newItem.classList.add(id);
+    newItem.innerHTML = productTemplate;
+    productContainer.appendChild(newItem);
+    handleCart(newItem);
+}
+// Handeling Cart
+
+const cart = document.querySelector(".cart");
+let cartList = [];
+let cartValue = 0;
+function handleCart(product) {
+    product.addEventListener("click", () => {
+        console.log(product);
+        let prodId = product.classList[1];
+        cartList.push(prodId);
+        console.log(cartList);
+
+        quantityCheck(product)
+
+        if (cartList.length > 0) {
+            document.querySelector("#empty").textContent = "";
+        }
+    });
+}
+// Make and append into the cart
+function makeCartItem(image, price, id, quantity) {
+    if(quantity>1){
+        const item = cart.querySelector("."+id);
+        const counter = item.querySelector(".counter");
+        counter.textContent = quantity;
+        return
+    }
+    const newCartItem = document.createElement("div");
+    newCartItem.classList.add("cart-item");
+    newCartItem.classList.add(id);
+    newCartItem.innerHTML = `
+    <div class="photo">
+        <img src="${image}" alt="Product Image">
+    </div>
+    <hr>
+    <p id="cart-price">₹ ${price}.00</p>
+    <div class="counter">${quantity}</div>
+    <div class="remove"><i class="fas fa-window-close"></i></div>
+    `;
+    cart.appendChild(newCartItem);
+    handleRemoval(newCartItem);
+}
+// If cross button is clicked the item from cart is removed
+function handleRemoval(product){
+    product.querySelector(".remove").addEventListener("click",()=>{
+        let target = product.classList[1];
+        while (cartList.includes(target)) {
+            let index = cartList.indexOf(target);
+            cartValue -= products[target].price;
+            cartList.splice(index, 1);
+        }
+        console.log(cartList)
+        console.log(cartValue)
+
+        updatePrice("0");
+        product.remove();
+    })
+}
+// To keep track of the price
+function updatePrice(price) {
+    cartValue += parseInt(price);
+    document.querySelector("#cost").textContent = "₹ " + cartValue + ".00";
+    document.querySelector("#total").textContent = "₹ " + cartValue + ".00";
+    let gst = cartValue * 0.08;
+    gst = gst.toFixed(2);
+    document.querySelector("#gst").textContent = "₹ " + gst;
+    document.querySelector("#discount").textContent = "-₹ " + gst;
+}
+// Handeling cart items quantity and the counter
+function quantityCheck(product){
+    let prodId = product.classList[1];
+
+    if (products[prodId]) {
+        const prod = products[prodId];
+        const prodImg = prod.image;
+        const prodPrice = prod.price;
+        let freq = 0;
+        cartList.forEach((i)=>{
+            if(i == prodId) freq++;
+        })
+        // Check if we have it in stock?
+        let inStock = parseInt(prod.quantity);
+        if(freq>inStock) {
+            alert("Sorry We ran out of stock :("); 
+            cartList.splice(cartList.indexOf(prodId),1)
+            return;
+        }
+        else if(freq==1) makeCartItem(prodImg, prodPrice, prodId, freq);
+        else makeCartItem(prodImg, prodPrice, prodId, freq);
+        updatePrice(prod.price);
+    }
+}
